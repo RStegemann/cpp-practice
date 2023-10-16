7e04227d86b783561bad72c2622171a629d64bff
Robin Stegemann <robinstegemann@hotmail.de>
2023-10-16 14:01:59 +0200

    Random number exercise

--- cpp-practice/cpp_practice.cpp
+++ cpp-practice/cpp_practice.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main()
 {
-    int number = rand();
-    std::cout << number;
+    // Random Numbers
+    const short min_value = 1;
+    const short max_value = 6;
+    srand(time(nullptr));
+    int die_1 = rand() % (max_value - min_value) + 1;
+    int die_2 = rand() % (max_value - min_value) + 1;
+    cout << "Die 1: " << die_1 << endl << "Die 2: " << die_2 << endl;
+    
 }
