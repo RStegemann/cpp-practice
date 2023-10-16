c49fdaf701cf018a08ccd4a3793795deab471a84
Robin Stegemann <robinstegemann@hotmail.de>
2023-10-16 14:23:53 +0200

    Cout formatting exercises

--- cpp-practice/cpp_practice.cpp
+++ cpp-practice/cpp_practice.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
@@ -12,5 +13,18 @@ int main()
     int die_1 = rand() % (max_value - min_value) + 1;
     int die_2 = rand() % (max_value - min_value) + 1;
     cout << "Die 1: " << die_1 << endl << "Die 2: " << die_2 << endl;
+
+    // Text Formatting
+    cout << left;
+    cout << setw(10) << "Spring" << "Nice" << endl
+         << setw(10) << "Summer" << "Hot" << endl;
+
+    // Double formatting
+    cout << fixed << setprecision(2) << 12.34567 << endl;
+
+    // Formatting Exercise
+    cout << left << setw(15) << "Course" << setw(15) << "Students" << endl;
+    cout << left << setw(15) << "C++" << right << setw(15) << "100" << endl;
+    cout << left << setw(15) << "JavaScript" << right << setw(15) << "50" << endl;
     
 }
